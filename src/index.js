56e51ed2a9fd28f860ece28368aea430885f0e8a
The Game <chanarusampath@gmail.com>
2018-04-13 11:57:44 +0530

    =Initialized the components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Titles from './components/Titles';
+import Form from './components/Form';
+import Weather from './components/Weather';
 
 class App extends React.Component {
     render() {
         return(
             <div>
                 Hello!
+                <Titles />
+                <Form />
+                <Weather />
             </div>
         );
     }
